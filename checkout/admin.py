4e1b99a02aae05ee2bff1c07cc91b580ceb85b11
Dissy Ulina <dissyulina@gmail.com>
2021-12-24 10:26:28 +0000

    Add order history to profile and update checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -18,7 +18,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'stripe_pid')
 
     # this will allow us to specify the order of the fields in the admin interface
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
